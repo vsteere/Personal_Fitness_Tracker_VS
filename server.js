079206a6fb9825e4c59a0dcaa26d45eea39ade91
vsteere <vssteere@gmail.com>
2020-10-30 13:38:03 -0500

    deploying

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  });
-
+  }
+);
 // API routes
 app.use(require("./routes/api-routes"));
 
